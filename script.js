3a6f33e75683152890160890369d75ced962c334
Helga Börjesson <helgaborjesson@gmail.com>
2022-11-28 14:54:04 +0100

    actions to the start buttons, added counting

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const imageStudent = document.querySelector('#image-student');
 const buttonStart = document.querySelector('#button-start-container');
 const buttonNames = document.querySelector('#button-container');
 const quizContainer = document.querySelector('#quiz-container');
-
-//let numberOfGuesse = 0;
+const result = document.querySelector('#result');
 
 // function to shuffle the students
 const shuffleArray = (array) => {
@@ -19,14 +18,16 @@ const shuffleArray = (array) => {
 	}
 };
 
-buttonTenStudents.innerHTML += `<button class="btn col-8 col-md-6 btn-outline-primary">10 students?</button>`;
-buttonTwentyStudents.innerHTML += `<button class="btn col-8 col-md-6 btn-outline-primary">20 students?</button>`;
-buttonAllStudents.innerHTML += `<button class="btn col-8 col-md-6 btn-outline-primary">All students?</button>`;
+buttonTenStudents.innerHTML += `<button class="btn col-8 col-md-6 btn-light">10 students</button>`;
+buttonTwentyStudents.innerHTML += `<button class="btn col-8 col-md-6 btn-light">20 students</button>`;
+buttonAllStudents.innerHTML += `<button class="btn col-8 col-md-6 btn-light">All students</button>`;
 
 
 
 //function to get a random student and 3 other names 
 const getRandomStudent = () => {
+	//this will keep it to four names, otherwise it will add for names every time you select a button/answer
+	buttonNames.innerHTML = "";
 	//calling the fuction to shuffle the students
 	shuffleArray(students); 
 	//slicing out 4 students and putting them in 'randomStudents'
@@ -41,10 +42,98 @@ const getRandomStudent = () => {
 	const randomNames = randomStudents.map(students => students.name); 
 	//adding a button for every name/student in randomNames
 	randomNames.forEach(student => {
-		buttonNames.innerHTML += `<button class="btn btn-outline-primary my-2">${student}</button>`;
+		buttonNames.innerHTML += `<button class="btn btn-outline-dark my-2">${student}</button>`;
 	});
-
 };
+
+
+// button, start quiz with 10 students
+buttonTenStudents.addEventListener('click', () => {
+	//hide buttons
+	buttonStart.style.display = 'none';
+	//call the function to get a random student and three random names
+	getRandomStudent();
+	//take out 10 students
+	const tenStudents = students.slice(0, 10);
+	console.log(tenStudents.length);
+});
+
+// button, start quiz with 20 students
+buttonTwentyStudents.addEventListener('click', () => {
+	//hide buttons
+	buttonStart.style.display = 'none';
+	//call the function to get a random student and three random names
+	getRandomStudent();
+	//take out twenty students
+	const twentyStudents = students.slice(0, 20);
+	console.log(twentyStudents.length);
+});
+
+// button, start quiz with all students
+buttonAllStudents.addEventListener('click', () => {
+	buttonStart.style.display = 'none';
+	//call the function to get a random student and three random names
+	getRandomStudent();
+	console.log(students.length);
+});
+
+
+
+
+
+
+let guesses = 0;
+let correctGuess = 0;
+let wrongGuess = 0;
+
+
+
+// Listen for guesses
+buttonNames.addEventListener('click', e => {
+	// Stop form from being sent to the server
+	e.preventDefault();
+
+	
+	//checking if someone is clicking the 'button'-element
+	if (e.target.tagName === "BUTTON") {
+		// Increase number of guesses made
+		guesses++;
+
+		// Check if guess was correct
+		if (e.target.innerText === correctStudent.name) {
+			correctGuess++;
+			console.log(`${correctStudent.name} is correct!`);
+
+		} else {
+			console.log(`${students.name} is wrong!`);
+		}
+		result.innerText = `${correctGuess} / ${guesses} correct`;
+		
+		getRandomStudent();
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //start the quiz, do you want to quiz with 10, 20 or all the students? button choices
@@ -83,13 +172,6 @@ console.log(getRandomStudent); */
 
 
 
-// get random 3 names
-/* const randomStudentNames = students.slice(1, 4);
-
-console.log("three names", randomStudentNames); */
-
-
-// shuffle all 4 names
 
 // show right or wrong answer
 
