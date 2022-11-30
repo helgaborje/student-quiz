fca690b86535af296243b80132876e433a1c8f51
Helga Börjesson <helgaborjesson@gmail.com>
2022-11-30 18:47:37 +0100

    trying to get more then 10 students to guess on...

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-/* const buttonTenStudents = document.querySelector('#button-10-students');
-const buttonTwentyStudents = document.querySelector('#button-20-students');
-const buttonAllStudents = document.querySelector('#button-all-students'); */
+/* 
+*
+* Inlämningsuppgift 1, JavaScript grundkurs - Helga Börjesson
+* 
+*/
 const imgContainer = document.querySelector('#image-container');
 const imageStudent = document.querySelector('#image-student');
 const buttonStart = document.querySelector('#button-start-container');
@@ -46,12 +48,14 @@ const getRandomStudent = () => {
 	correctStudent = arrayStudents[0]; 
 	//image of the student
 	imageStudent.setAttribute('src', correctStudent.image);
+	console.log(correctStudent);
 
 	//shuffeling the 4 'randomStudents'
 	shuffleArray(randomStudents); 
 
 	//filtring out the students that has not yet been shown
 	arrayStudents = arrayStudents.filter((student) => student.name !== correctStudent.name);
+	console.log(arrayStudents)
 
 	//using map() to pick out the 4 names that are saved in 'randomStudents', to a new const named 'randomNames' 
 	const randomNames = randomStudents.map(students => students.name); 
@@ -68,7 +72,16 @@ document.querySelector('#button-10-students').addEventListener('click', () => {
 	buttonStart.style.display = 'none';
 
 	getRandomStudent();
+	
 
+});
+
+// button, start quiz with 10 students
+document.querySelector('#button-20-students').addEventListener('click', () => {
+	//hide buttons
+	buttonStart.style.display = 'none';
+
+	getRandomStudent();
 
 });
 
@@ -84,12 +97,10 @@ buttonNames.addEventListener('click', e => {
 	// Stop form from being sent to the server
 	e.preventDefault();
 
-	
 	//checking if someone is clicking the 'button'-element
 	if (e.target.tagName === "BUTTON") {
 		// Increase number of guesses made
 		guesses++;
-		
 
 		// Check if guess was correct
 		if (e.target.innerText === correctStudent.name) {
@@ -108,19 +119,16 @@ buttonNames.addEventListener('click', e => {
 				<button onclick="window.location.reload();" class="btn my-2 col-8 col-md-6 btn-light">
 				Spela igen
 				</button>
-				
 				`;
-
 			}
 
 		} else {
 
-		console.log(`${tenStudents.name} is wrong!`);
 		} 
 		result.innerText = `${correctGuess} / ${guesses} correct`;
 
 		getRandomStudent();
-		
+
 	}
 });
 
